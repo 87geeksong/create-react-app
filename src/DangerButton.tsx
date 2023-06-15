691b0801f85795d4553025168f3b00f725c1421d
ws <@ws870762>
2023-06-15 19:12:31 +0800

    git init

---
+++ src/DangerButton.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Buttons from '@/components/Buttons';
+
+class DangerButton extends Component {
+  render() {
+    return <Buttons />;
+  }
+}
+
+export default DangerButton;
