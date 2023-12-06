ef5c114d699fee415576f908d49fe9c11859d0c5
ws <@ws870762>
2023-12-06 18:35:14 +0800

    opt: router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
 import React from "react";
-import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Routes, useNavigate } from "react-router-dom";
 import router from "./router";
 import { Tree } from "antd";
 const { TreeNode } = Tree;
 function App() {
-  console.log(router,'rr');
-  
+  const treeData = []
+  const navigate = useNavigate()
+  router.map((item,index)=>{
+    const children = []
+    item.children.map((v, i)=>{
+      children.push({
+        title: v.title,
+        key: `${index}-${i}`,
+        path: v.path,
+      })
+    })
+    treeData.push({
+      title: item.title,
+      key: index,
+      children
+    })
+  })
+  console.log(useNavigate,'useNavigate');
+  const onExpand = (expandedKeys, obj) => {
+    console.log(expandedKeys, obj, 1111111);
+    const {node: {path}} = obj
+    console.log(path,'rr');
+    navigate(path)
+  };
   return (
-    <></>
+    <>
+      <Tree
+        selectable
+        onSelect={onExpand}
+      >
+        {treeData.map((item) => {
+          return (
+            <TreeNode key={item.key} title={item.title}>
+              {!!item.children.length &&
+                item.children.map((v) => {
+                  return <TreeNode path={v.path} key={v.key} title={v.title}/>
+                })}
+            </TreeNode>
+          );
+        })}
+      </Tree>
+      <Routes>
+        {!!router &&
+          router.map((item, index) => {
+            return item.children.map((v,i)=>{
+              return (
+                <Route
+                  key={`${index}-${i}`}
+                  path={v.path}
+                  element={<v.component />}
+                  exact={v.exact}
+                />
+              );
+            })
+          })}
+      </Routes>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <App />
+  <Router>
+    <App />
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
